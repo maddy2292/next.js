499fed5580873fedf881b3847b543db372006913
Mandy Lehmann <mandy.lehmann@adesso.de>
2024-05-17 16:15:05 +0200

    add cms for content

--- content/reviews/hollow-knight.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hollow Knight"
-date: "2023-08-27"
-image: "/images/hollow-knight.jpg"
----
-
-__Hollow Knight__ ist ein Computerspiel aus dem Metroidvania-Genre, welches von Team Cherry entwickelt und 2017 für Microsoft Windows, Linux und macOS und 2018 für Xbox One, PlayStation 4 und Nintendo Switch veröffentlicht wurde.[1] Die Entwicklung wurde hauptsächlich durch die Crowdfundingplattform Kickstarter.com finanziert.
--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,40 +1,79 @@
-import { readdir, readFile } from 'node:fs/promises';
 import { marked } from 'marked';
-import  matter from 'gray-matter';
+import qs from 'qs';
 
-export interface Review {
-    slug: string;
-    title: string;
-    date: string;
-    image: string;
-    body: string;
+const CMS_URL = 'http://localhost:1337';
+
+interface CmsItem {
+  id: number;
+  attributes: any;
 }
 
-export async function getFeaturedReview() {
+export interface Review {
+  slug: string;
+  title: string;
+  date: string;
+  image: string;
+}
+
+export interface FullReview extends Review {
+  body: string;
+}
+
+export async function getFeaturedReview(): Promise<Review> {
   const reviews = await getReviews();
   return reviews[0];
 }
 
-export async function getReview(slug: string): Promise<Review> {
-    const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
-    const {content, data: {title, date, image} } = matter(text);
-    const body = marked(text);
-    return {slug, title, date, image, body};
+export async function getReview(slug: string): Promise<FullReview> {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
+    populate: { image: { fields: ['url'] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
+  return {
+    ...toReview(item),
+    body: marked(item.attributes.body),
+  };
 }
 
 export async function getReviews(): Promise<Review[]> {
-    const slugs = await getSlugs();
-    const reviews: Review[] = [];
-    for (const slug of slugs) {
-      const review = await getReview(slug);
-      reviews.push(review);
-    }
-    reviews.sort((a, b) => b.date.localeCompare(a.date));
-    return reviews;
+  const { data } = await fetchReviews({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: { image: { fields: ['url'] } },
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6 },
+  });
+  return data.map(toReview);
 }
 
-export async function getSlugs() {
-  const files = await readdir('./content/reviews');
-  return files.filter((file) => file.endsWith('.md'))
-      .map((file) => file.slice(0, -'.md'.length));
+export async function getSlugs(): Promise<string[]> {
+  const { data } = await fetchReviews({
+    fields: ['slug'],
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 100 },
+  });
+  return data.map((item: CmsItem) => item.attributes.slug);
+}
+
+async function fetchReviews(parameters: any) {
+  const url = `${CMS_URL}/api/reviews?`
+    + qs.stringify(parameters, { encodeValuesOnly: true });
+  // console.log('[fetchReviews]:', url);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return await response.json();
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
 }
